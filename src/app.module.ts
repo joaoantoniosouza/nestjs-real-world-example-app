3a440132b6897748fb160c9de37b53d3a00e9a53
joao <jasouza96@gmail.com>
2021-05-30 01:09:47 -0300

    feat: configurando aquivos de ambiente

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const ENV = process.env.NODE_ENV;
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env.${ENV}.local`, `.env.${ENV}`, '.env'],
+      isGlobal: true,
+      cache: true,
+      expandVariables: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
