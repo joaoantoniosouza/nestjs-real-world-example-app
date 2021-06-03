f4a3e800cc8ec3e6a9b7b77b89c4c87ca3f19fc9
joao <jasouza96@gmail.com>
2021-06-03 17:50:20 -0300

    feat: tipando método chekPassword

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,4 @@
-import {
-  Injectable,
-  NotFoundException,
-  UnauthorizedException,
-} from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { UserCreateDTO, UserUpdateDTO } from './dto';
@@ -62,7 +58,7 @@ export class UserService {
     return null;
   }
 
-  private async checkPassword(password, passwordHash) {
+  private async checkPassword(password: string, passwordHash: string) {
     return this.hashTool.isMatch(password, passwordHash);
   }
 
