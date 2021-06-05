c59fa8fbac411c14ab10e6d93a8ba70b6c1bfff4
joao <jasouza96@gmail.com>
2021-06-05 15:59:53 -0300

    feat: exportando jwtAuthGuard

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,6 +3,7 @@ import { ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { HashTool } from '~/shared/security/hash';
+import { JwtAuthGuard } from './guards/jwt.guard';
 import { UserController } from './user.controller';
 import { UserEntity } from './user.entity';
 import { UserService } from './user.service';
@@ -16,7 +17,7 @@ import { UserService } from './user.service';
       inject: [ConfigService],
     }),
   ],
-  providers: [UserService, HashTool],
+  providers: [UserService, HashTool, JwtAuthGuard],
   controllers: [UserController],
   exports: [UserService, JwtModule],
 })
