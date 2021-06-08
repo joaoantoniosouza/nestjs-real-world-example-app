0e3661b5b4602d186327e760bc8d1b830fbf12be
joao <jasouza96@gmail.com>
2021-06-08 11:05:32 -0300

    feat: criando configuração de produçao

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,11 +1,24 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import * as path from 'path';
 
-export const typeormConfig: TypeOrmModuleOptions = {
+export const developmentConfig: TypeOrmModuleOptions = {
   type: 'sqlite',
   database: path.resolve(__dirname, '..', '..', 'db', 'data.db3'),
+  logging: 'all',
+};
+
+const productionConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  url: process.env.DATABASE_URL,
+  logging: false,
+};
+
+const configToMerge =
+  process.env.NODE_ENV === 'production' ? productionConfig : developmentConfig;
+
+export const typeormConfig: TypeOrmModuleOptions = {
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: true,
   logger: 'advanced-console',
-  logging: 'all',
+  ...configToMerge,
 };
