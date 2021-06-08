046c9e6db7646977bfac5a0cca323d03996f5abf
joao <jasouza96@gmail.com>
2021-06-08 15:07:20 -0300

    style: removendo dependência não utilizada

--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -1,8 +1,4 @@
-import {
-  BadRequestException,
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { UserEntity } from '~/user/user.entity';
