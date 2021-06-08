32d8ead356fbd5a531bdc5cc3f1cb3b0a2db5414
joao <jasouza96@gmail.com>
2021-06-08 11:05:02 -0300

    feat: setando api prefix para 'api'

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
@@ -5,6 +6,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
+  app.setGlobalPrefix('api');
 
   await app.listen(configService.get('application.port'));
 }
